6ea393850b88625ee72b1b24b023506e6f7efae6
Bharath <bharathsurya021@gmail.com>
2022-06-12 13:58:35 +0530

    win logic and state lifting

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,26 +1,7 @@
 import React, { useState } from 'react'
 import Square from './Square'
-const Board = () => {
-    const [board, setBoard] = useState(Array(9).fill(null)
-    )
-    const [isXNext, setIsXNext] = useState(false)
-    console.log(board);
-    const handleSquareClick = (position) => {
-        if (board[position]) {
-            return
-        }
+const Board = ({ board, handleSquareClick }) => {
 
-        setBoard((prev) => {
-            return (prev.map((square, pos) => {
-                if (pos === position) {
-                    return isXNext ? 'X' : 'O';
-                }
-                return square;
-            }))
-        })
-
-        setIsXNext((prev) => !prev)
-    }
     const renderSquare = (position) => {
         return (
             <Square value={board[position]} onClick={() => {
