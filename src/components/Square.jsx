70de2cc507801931822ca972ac807472576bc752
Bharath <bharathsurya021@gmail.com>
2022-06-12 11:59:28 +0530

    add styles

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
-
 const Square = ({ value }) => {
     return (
         <>
-            <button type='button'>{value}</button>
+            <button className='square' type='button'>{value}</button>
         </>
     )
 }
