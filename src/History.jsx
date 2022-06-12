f472b93c329ea168fade8297ea98faca3f8bb468
Bharath <bharathsurya021@gmail.com>
2022-06-12 20:36:41 +0530

    display game history

---
+++ src/History.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const History = ({ history, currentMove, moveTo }) => {
+    return (
+        <ul>
+            {history.map((_, move) => {
+                return <li key={move}>
+                    <button type='button' onClick={() => {
+                        moveTo(move)
+                    }} style={{ fontWeight: (move === currentMove) ? 'bold' : 'normal' }} >
+                        {move === 0 ? 'Go to game start' : `Go to move #${move}`}
+                    </button>
+                </li>
+            })}
+
+        </ul>
+    )
+}
+
+export default History
